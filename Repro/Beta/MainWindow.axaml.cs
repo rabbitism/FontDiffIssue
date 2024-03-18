4c6e936795c40626a1b5bc08c3598618334491fc
rabbitism <popmessiah@hotmail.com>
2024-03-18 20:26:13 +0800

    feat: add demo.

---
+++ Repro/Beta/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Beta;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Repro/Stable/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Stable;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
